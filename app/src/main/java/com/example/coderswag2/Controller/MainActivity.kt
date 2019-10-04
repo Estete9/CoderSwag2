9bbebcb67a0dd74aa95aa8c978913d11b53953a2
Esteban P <cepalacios@outlook.com>
2019-10-04 08:50:29 -0500

    linked the custom adapter with the layout

--- app/src/main/java/com/example/coderswag2/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag2/Controller/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.coderswag2.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.BaseAdapter
+import com.example.coderswag2.Adapters.CategoryAdapter
 import com.example.coderswag2.R
+import com.example.coderswag2.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+ lateinit var  adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
