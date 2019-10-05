3874234c176fed34813fff8e16ff1c4b0dfa1f55
Esteban P <cepalacios@outlook.com>
2019-10-05 07:23:52 -0500

    complete list view with custom adapter and onclickitem listener

--- app/src/main/java/com/example/coderswag2/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag2/Controller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.coderswag2.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.BaseAdapter
+import android.widget.Toast
 import com.example.coderswag2.Adapters.CategoryAdapter
+import com.example.coderswag2.Model.Category
 import com.example.coderswag2.R
 import com.example.coderswag2.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val Category=DataService.categories[i]
+            Toast.makeText(this,"clicked on ${Category.title} cell", Toast.LENGTH_SHORT).show()
+        }
     }
 }
